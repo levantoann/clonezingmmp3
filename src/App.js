9747087649df5f4d187315bc48ba37e3ac9a9203
levantoann <lvtoan.19it4@vku.udn.vn>
2023-06-27 15:37:34 +0700

    'v2'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
 
 
 function App() {
+  const [work,setWord] = useState('')
+  console.log(work)
   return (
-    <div className="root">
-      app
+    <div className="flex h-screen gap-8 items-center border border-red-500 justify-center">
+      <input type="text" className="outline-none border border-blue-600 px-4 py-2 w-[400px]" value={work}
+        onChange ={e => setWord(e.target.value)}/>
+      <button type="button" className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white">Thêm</button>
     </div>
   );
 }
